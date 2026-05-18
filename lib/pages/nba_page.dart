538bf32d5d967f50f834e286bd17b59ae52a3cdb
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-18 23:00:19 +0600

    Save the data in teams list of Team. the api is providing 25 different city and abbreviation. it is all saved in the teams list

--- lib/pages/nba_page.dart
+++ lib/pages/nba_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:nbaapp/model/team.dart';
 
 class NbaPage extends StatefulWidget {
   const NbaPage({super.key});
@@ -15,13 +18,23 @@ class _NbaPageState extends State<NbaPage> {
     getTeams();
   }
 
+  //final
+  List<Team> teams = [];
+
   Future getTeams() async {
     final response = await http.get(
       Uri.https('api.balldontlie.io', '/nba/v1/games'),
       headers: {'Authorization': '690578ec-8536-44c0-8b23-fb435becb89c'},
     );
-    print("${response.statusCode}");
-    print(response.body);
+    var jsonBody = jsonDecode(response.body);
+    for (var eachTeam in jsonBody['data']) {
+      final team = Team(
+        city: eachTeam?['city'],
+        abbreviation: eachTeam?['abbreviation'],
+      );
+      teams.add(team);
+    }
+    print(teams.length);
   }
 
   @override
