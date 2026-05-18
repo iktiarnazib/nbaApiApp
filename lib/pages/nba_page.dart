f6580f91587e055f612e0df7d98321b53322b12c
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-18 17:45:36 +0600

    Nba app initialization and added page

---
+++ lib/pages/nba_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NbaPage extends StatelessWidget {
+  const NbaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Home'), centerTitle: true));
+  }
+}
